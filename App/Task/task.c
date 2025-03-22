e007f2b1154959711acef254281190106a72ed51
nathan.guan <nathanweasley.work@gmail.com>
2025-03-22 12:59:46 +0800

    added gpio flag for gui_drawing profilling

--- App/Task/task.c
+++ App/Task/task.c
@@ -8,13 +8,28 @@
 
 void task_sensor()
 {
+    LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
     uint8_t * img = NULL;
     img = EPD_GetVRAM();
 
+    GPIO_InitStruct.Pin = LL_GPIO_PIN_2;
+    GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+    GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+    GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+    LL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+    GPIOB->BRR = LL_GPIO_PIN_2;
+
+    GPIOB->BSRR = LL_GPIO_PIN_2;
     EPD_Init();
+    GPIOB->BRR = LL_GPIO_PIN_2;
+    GPIOB->BSRR = LL_GPIO_PIN_2;
     EPD_Clear();
+    GPIOB->BRR = LL_GPIO_PIN_2;
 
     LL_mDelay(500);
+
+    GPIOB->BSRR = LL_GPIO_PIN_2;
 
     /** Paint black part */
     Paint_NewImage(img, EPD_WIDTH, EPD_HEIGHT, 90, WHITE);
@@ -28,6 +43,9 @@ void task_sensor()
     Paint_DrawCircle(125, 85, 15, BLACK, DOT_PIXEL_1X1, DRAW_FILL_EMPTY);
     EPD_UpdateBlack(img);
 
+    GPIOB->BRR = LL_GPIO_PIN_2;
+    GPIOB->BSRR = LL_GPIO_PIN_2;
+
     /** Paint red part */
     Paint_Clear(WHITE);
     Paint_DrawPoint(5, 90, RED, DOT_PIXEL_3X3, DOT_STYLE_DFT);
@@ -39,6 +57,8 @@ void task_sensor()
     Paint_DrawString_EN(5, 0, "waveshare Electronics", &Font12, BLACK, WHITE);
     Paint_DrawNum(5, 50, 987654321, &Font16, WHITE, RED);
     EPD_UpdateRed(img);
+
+    GPIOB->BRR = LL_GPIO_PIN_2;
 
     /** Update display */
     EPD_TurnOnDisplay();
